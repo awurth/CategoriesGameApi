1d88e8e30299449e809c9858a734f1e442f5cc6d
Alexis Wurth <alexis.wurth57@gmail.com>
2017-02-23 16:13:04 +0100

    Set application routes

--- bootstrap/routes.php
+++ bootstrap/routes.php
@@ -2,6 +2,11 @@
 
 use App\Service\RestRouter;
 
+use App\Middleware\AuthMiddleware;
+
+use App\Controller\SubjectController;
+use App\Controller\GameController;
+
 $router = new RestRouter($container['router'], $config['rest']);
 
 /**
@@ -18,48 +23,11 @@ $app->group('/api', function () use ($container) {
     $this->post('/register', 'AuthController:register')->setName('register');
     $this->post('/login', 'AuthController:login')->setName('login');
     $this->get('/users/me', 'AuthController:me')
-        ->add(new App\Middleware\AuthMiddleware($container))
+        ->add(new AuthMiddleware($container))
         ->setName('users.me');
 });
 
-/**
- *         URL          |           CONTROLLER            |     ROUTE
- * ---------------------|---------------------------------|----------------
- * GET /articles        | ArticleController:getArticle    | get_articles
- * GET /articles/{id}   | ArticleController:getArticles   | get_article
- * POST /articles       | ArticleController:postArticle   | post_article
- * PUT /articles/{id}   | ArticleController:putArticle    | put_article
- * DELETE /article/{id} | ArticleController:deleteArticle | delete_article
- */
-$router->CRUD('articles', 'ArticleController');
+$router->CRUD('subjects', SubjectController::class);
+$games = $router->CRUD('games', GameController::class);
 
-// OR
-
-// $router->cget('articles', 'ArticleController');
-// $router->get('articles', 'ArticleController');
-// $router->post('articles', 'ArticleController');
-// $router->put('articles', 'ArticleController');
-// $router->delete('articles', 'ArticleController');
-
-/***********************************************************/
-/* -------------------- SUB RESOURCES -------------------- */
-/***********************************************************/
-
-/**
- *                        URL                         |                   CONTROLLER                  |        ROUTE
- * ---------------------------------------------------|-----------------------------------------------|------------------------
- * GET /articles/{article_id}/comments                | ArticleCommentController:getArticleComments   | get_article_comments
- * GET /articles/{article_id}/comments/{comment_id}   | ArticleCommentController:getArticleComment    | get_article_comment
- * POST /articles/{article_id}/comments               | ArticleCommentController:postArticleComment   | post_article_comment
- * PUT /articles/{article_id}/comments/{comment_id}   | ArticleCommentController:putArticleComment    | put_article_comment
- * DELETE /article/{article_id}/comments/{comment_id} | ArticleCommentController:deleteArticleComment | delete_article_comment
- */
-$router->subCRUD('articles', 'comments', 'ArticleCommentController');
-
-// OR
-
-// $router->cgetSub('articles', 'comments', 'ArticleController');
-// $router->getSub('articles', 'comments', 'ArticleController');
-// $router->postSub('articles', 'comments', 'ArticleController');
-// $router->putSub('articles', 'comments', 'ArticleController');
-// $router->deleteSub('articles', 'comments', 'ArticleController');
+$games['post']->add(new AuthMiddleware($container));
