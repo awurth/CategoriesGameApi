60c70d747524f2068ace91c62bc881fef43a5ec3
Alexis Wurth <alexis.wurth57@gmail.com>
2017-02-26 18:10:02 +0100

    Updated handlers

--- bootstrap/handlers.php
+++ bootstrap/handlers.php
@@ -1,9 +1,11 @@
 <?php
 
 use Slim\Handlers\Strategies\RequestResponseArgs;
+use Slim\Exception\NotFoundException;
+use App\Exception\AccessDeniedException;
 
 /**
- * Return error in JSON if when a NotFoundException is thrown
+ * Return error in JSON when a NotFoundException is thrown
  */
 $container['notFoundHandler'] = function ($container) {
     return function ($request, $response) use ($container) {
@@ -21,18 +23,45 @@ $container['notFoundHandler'] = function ($container) {
  */
 $container['notAllowedHandler'] = function ($container) {
     return function ($request, $response, $methods) use ($container) {
+        $allowedMethods = implode(', ', $methods);
+
+        if ($allowedMethods === 'OPTIONS') {
+            throw new NotFoundException($request, $response);
+        }
+
         return $response
             ->withStatus(405)
-            ->withHeader('Allow', implode(', ', $methods))
+            ->withHeader('Allow', $allowedMethods)
             ->withJson([
                 'status' => 405,
-                'message' => 'Method must be one of: ' . implode(', ', $methods)
+                'message' => 'Method must be one of: ' . $allowedMethods
             ]);
     };
 };
 
+/**
+ * Return error in JSON when an AccessDeniedException is thrown
+ */
+$container['accessDeniedHandler'] = function ($container) {
+    return function ($request, $response, $exception) use ($container) {
+        return $response
+            ->withStatus(403)
+            ->withJson([
+                'status' => 403,
+                'message' => $exception->getMessage()
+            ]);
+    };
+};
+
+/**
+ * Default Slim error handler
+ */
 $container['errorHandler'] = function ($container) use ($config) {
     return function ($request, $response, $exception) use ($container, $config) {
+        if ($exception instanceof AccessDeniedException) {
+            return $container['accessDeniedHandler']($request, $response, $exception);
+        }
+
         $message = [
             'status' => 500,
             'message' => 'Internal Server Error'
@@ -49,6 +78,9 @@ $container['errorHandler'] = function ($container) use ($config) {
     };
 };
 
+/**
+ * PHP error handler
+ */
 $container['phpErrorHandler'] = function ($container) use ($config) {
     return function ($request, $response, $error) use ($container, $config) {
         $message = [
